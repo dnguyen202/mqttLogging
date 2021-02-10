3a43c1f859756b2cc41acf86b04aed1325692121
Destiny Nguyen <dnguyen@ubicquia.com>
2021-02-10 10:46:22 -0500

    test receiving tx commands

--- mqttLogging.py
+++ mqttLogging.py
@@ -2,9 +2,12 @@ import paho.mqtt.client as mqtt
 import codecs
 import subprocess
 from datetime import datetime, date
+import base64
 
 # global variable to retrieve mqtt message outside of on_message function
 global_encoded_pMsg = ''
+# broker/server for mqtt
+server = 'mqtt.test7.ubicquia.com'
 
 EUI = 'a4241089bb6a942f'
 
@@ -19,20 +22,27 @@ def on_connect(client, userdata, flags, rc):
     else:
         print('Connection unsuccessful, return code: ', str(rc))
 
+# callback if mqtt connection gets disconnected
+def on_disconnect(client, userdata, flags, rc=0):
+    print('Disconnected, return code: ', str(rc))
+
 # callback to receive messages
 def on_message(client, userdata, msg):
     global global_encoded_pMsg
     print('msg received')
     # encode to hex, then decode to utf-8 to get rid of the 'b' bytes
     global_encoded_pMsg = str(codecs.encode(msg.payload, "hex").decode("utf-8"))
-    decode_mqtt()
+    print(f'look here: {global_encoded_pMsg}')
+    # log and decode the received mqtt message
+    log_mqtt()
 
 # decode and writes to log file
-def decode_mqtt():
+def log_mqtt():
     # current date and time
     now = datetime.now()
     today = date.today()
     curr_time = now.strftime("%H:%M:%S")
+
     # set filename to eui with today's date
     # this will create a new log file every day
     log_file = open(EUI + '-mqttLog-%s.txt' % today, 'a')
@@ -40,8 +50,10 @@ def decode_mqtt():
     log_file.write(f'{curr_time}\n')
     # otherwise time will print after mqtt message
     log_file.flush()
+
     cmd = "node ubitools/ubilpp-decode.js " + global_encoded_pMsg
     subprocess.run(cmd, shell=True, stdout=log_file)
+
     log_file.close()
 
 
@@ -52,12 +64,13 @@ def ubicellMQTT():
 
     # callback functions
     client.on_connect = on_connect
+    client.on_disconnect = on_disconnect
     # uncomment below to see buffer messages
     # client.on_log = on_log
     client.on_message = on_message
 
-    # connecting to test7
-    client.connect('mqtt.test7.ubicquia.com')
+    # connecting to server
+    client.connect(server)
 
     CMD_topic = 'ubicell/%s/tx' % EUI
     STATUS_topic = 'ubicell/%s/rx' % EUI
@@ -66,6 +79,7 @@ def ubicellMQTT():
     client.subscribe(CMD_topic)
     # subscribe to status topic
     client.subscribe(STATUS_topic)
+
     # listen to node forever
     client.loop_forever()
 
