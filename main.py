1f5eaecdf4f26976af5d3633d4106367001d32f4
Destiny Nguyen <dnguyen@ubicquia.com>
2021-02-09 21:56:10 -0500

    logging mqtt

--- main.py
+++ main.py
@@ -1,44 +1,47 @@
 import paho.mqtt.client as mqtt
-import time
 import codecs
 import subprocess
-from pathlib import Path
-from datetime import datetime
+from datetime import datetime, date
 
 # global variable to retrieve mqtt message outside of on_message function
 global_encoded_pMsg = ''
 
 EUI = 'a4241089bb6a942f'
 
-
+# print out log buffer
 def on_log(client, userdata, level, buffer):
     print(f'log: {buffer}')
 
-
+# will let user know if connection is successful to MQTT
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         print('Connection successful')
     else:
         print('Connection unsuccessful, return code: ', str(rc))
 
-
-def on_disconnect(client, userdata, flags, rc=0):
-    print('Disconnected, return code: ', str(rc))
-
-
+# callback to receive messages
 def on_message(client, userdata, msg):
     global global_encoded_pMsg
-    # print(f'topic: {msg.topic}')
+    print('msg received')
     # encode to hex, then decode to utf-8 to get rid of the 'b' bytes
     global_encoded_pMsg = str(codecs.encode(msg.payload, "hex").decode("utf-8"))
+    decode_mqtt()
 
-    return global_encoded_pMsg
-
-
+# decode and writes to log file
 def decode_mqtt():
     global global_encoded_pMsg
-    cmd = "node /home/destiny/ubitools/ubilpp-decode.js " + global_encoded_pMsg
-    log_file = open('mqttLog.txt', 'a')
+    # current date and time
+    now = datetime.now()
+    today = date.today()
+    curr_time = now.strftime("%H:%M:%S")
+    # set filename to eui with today's date
+    # this will create a new log file every day
+    log_file = open(EUI + '-mqttLog-%s.txt' % today, 'a')
+    # add current time stamp before each mqtt message
+    log_file.write(f'{curr_time}\n')
+    # otherwise time will print after mqtt message
+    log_file.flush()
+    cmd = "node ubitools/ubilpp-decode.js " + global_encoded_pMsg
     subprocess.run(cmd, shell=True, stdout=log_file)
     log_file.close()
 
@@ -48,31 +51,24 @@ def ubicellMQTT():
     # constructor: creating a client
     client = mqtt.Client()
 
-    # creating instances
+    # callback functions
     client.on_connect = on_connect
-    client.on_disconnect = on_disconnect
-    client.on_log = on_log
+    # uncomment below to see buffer messages
+    # client.on_log = on_log
     client.on_message = on_message
 
-    # connecting to test5
+    # connecting to test7
     client.connect('mqtt.test7.ubicquia.com')
-    print('connected')
-    # begin async subscription
-    client.loop_start()
 
     CMD_topic = 'ubicell/%s/tx' % EUI
     STATUS_topic = 'ubicell/%s/rx' % EUI
-    # listening to nodes
+
     # subscribe to command topic
     client.subscribe(CMD_topic)
     # subscribe to status topic
     client.subscribe(STATUS_topic)
-    while True:
-        decode_mqtt()
-
-    # end async subscription
-    client.loop_stop()
-    client.disconnect()
+    # listen to node forever
+    client.loop_forever()
 
 
 # for testing
